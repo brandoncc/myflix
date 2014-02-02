b3ee3ebde6d8de9971c644c2a99e8ff2b0dbcb23
Brandon Conway <brandoncc@gmail.com>
2014-02-02 08:11:22 -0800

    move sorting and rating business logic back to the model

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -18,11 +18,12 @@ class UsersController < ApplicationController
   end
 
   def update_queue
-    if reorder_queue_items && update_queue_item_ratings
+    if current_user.reorder_queue_items(queue_items_with_valid_position) &&
+        current_user.update_queue_item_ratings(queue_items_with_valid_rating)
       flash[:success] = 'Your queue has been updated successfully.'
     else
       flash[:danger] =
-      'There was a problem updating your queue. Please try again.'
+        'There was a problem updating your queue. Please try again.'
     end
 
     redirect_to :back
@@ -30,62 +31,12 @@ class UsersController < ApplicationController
 
   private
 
-  def reorder_queue_items
-    transaction_was_successful = true
-
-    current_user.transaction do
-      queue_items_in_params_with_parameter(:position).each do |id, parameters|
-        queue_item = QueueItem.find(id)
-        if parameters[:position] =~ /\A\d+\z/ && queue_item.user == current_user
-          queue_item.position = parameters[:position]
-          queue_item.save
-        else
-          transaction_was_successful = false
-          raise ActiveRecord::Rollback
-        end
-      end
-      current_user.sort_queue_items_by_position
-    end
-
-    transaction_was_successful
+  def queue_items_with_valid_rating
+    params[:user][:queue_item].select{|_,v|v.include?(:rating)}.delete_if{|_,v| v[:rating].blank?}
   end
 
-  def update_queue_item_ratings
-    transaction_was_successful = true
-
-    queue_items_in_params_with_valid_rating = queue_items_in_params_with_parameter(:rating)
-
-    queue_items_in_params_with_valid_rating.delete_if{|_,v| v[:rating].blank?}
-
-    params[:user][:queue_item].select{|_,v|v.include?(:rating) && (1..5).include?(v['rating'].to_i)}
-
-    current_user.transaction do
-      queue_items_in_params_with_valid_rating.each do |id, parameters|
-        if Review.valid_ratings.include?(parameters[:rating].to_i)
-          queue_item = QueueItem.find(id)
-          review = Review.find_by_creator_and_video(current_user, queue_item.video)
-          if review
-            review.rating = parameters[:rating]
-          else
-            review = Review.new(creator: current_user, video: queue_item.video, rating: parameters[:rating])
-          end
-
-          if !review.save(validate: false)
-            transaction_was_successful = false
-            raise ActiveRecord::Rollback
-          end
-        else
-          transaction_was_successful = false
-          raise ActiveRecord::Rollback
-        end
-      end
-    end
-
-    transaction_was_successful
-  end
-
-  def queue_items_in_params_with_parameter(parameter)
-    params[:user][:queue_item].select{|_,v|v.include?(parameter)}
+  def queue_items_with_valid_position
+    params[:user][:queue_item].select{|_,v|v.include?(:position)}
   end
 
   def user_params
--- app/models/user.rb
+++ app/models/user.rb
@@ -8,9 +8,59 @@ class User < ActiveRecord::Base
   has_secure_password validations: false
   validates_presence_of :password
 
+  def reorder_queue_items(items)
+    transaction_was_successful = true
+
+    ActiveRecord::Base.transaction do
+      items.each do |id, parameters|
+        queue_item = QueueItem.find(id)
+        if parameters[:position] =~ /\A\d+\z/ && queue_item.user == self
+          queue_item.position = parameters[:position]
+          queue_item.save
+        else
+          transaction_was_successful = false
+          raise ActiveRecord::Rollback
+        end
+      end
+      sort_queue_items_by_position
+    end
+
+    transaction_was_successful
+  end
+
+  def update_queue_item_ratings(items)
+    transaction_was_successful = true
+
+    ActiveRecord::Base.transaction do
+      items.each do |id, parameters|
+        if Review.valid_ratings.include?(parameters[:rating].to_i)
+          queue_item = QueueItem.find(id)
+          review = Review.find_by_creator_and_video(self, queue_item.video)
+          if review
+            review.rating = parameters[:rating]
+          else
+            review = Review.new(creator: self, video: queue_item.video, rating: parameters[:rating])
+          end
+
+          if !review.save(validate: false)
+            transaction_was_successful = false
+            raise ActiveRecord::Rollback
+          end
+        else
+          transaction_was_successful = false
+          raise ActiveRecord::Rollback
+        end
+      end
+    end
+
+    transaction_was_successful
+  end
+
+  private
+
   def sort_queue_items_by_position
-    self.break_position_ties
-    self.fill_queue_item_position_gaps
+    break_position_ties
+    fill_queue_item_position_gaps
   end
 
   def break_position_ties
