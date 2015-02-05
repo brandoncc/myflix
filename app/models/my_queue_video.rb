8ecc13a3b09e01ec079721174990afed818cf8cc
figochen <figocia@hotmail.com>
2015-02-06 09:22:07 +1100

    fix destroy using new local variable name for new reivew

--- app/models/my_queue_video.rb
+++ app/models/my_queue_video.rb
@@ -11,30 +11,35 @@ class MyQueueVideo < ActiveRecord::Base
   end
 
   def rating
-    review = Review.where(user_id: user.id, video_id: video.id).first    
+    # review = Review.where(user_id: user.id, video_id: video.id).first    
     review.rating if review
 
   end
 
   def rating=(new_rating)    
     # binding.pry
-    review = Review.where(user_id: user.id, video_id: video.id).first    
-    if new_rating
+    
+    if !new_rating.blank?
+      # 
       if review
+        # binding.pry
         review.update_column(:rating, new_rating)
       else
-        review = Review.new(rating: new_rating, user: user, video: video)
-        review.save(validate: false)
+        new_review = Review.new(rating: new_rating, user: user, video: video)
+        new_review.save(validate: false)
       end
     else      
+      # require 'pry';binding.pry
        review.destroy if review
     end
     
   end
 
 
-  # private 
-  # def review
-  #   @review ||= Review.where(user_id: user.id, video_id: video.id).first
-  # end
+   
+  private
+  def review
+    @review ||= Review.where(user_id: user.id, video_id: video.id).first
+  end
+
 end
