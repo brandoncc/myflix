400fddf3e25d759634e035fd8a7fd378a38d79e2
figochen <figocia@hotmail.com>
2015-04-21 09:03:34 +1000

    seed

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-# This file should contain all the record creation needed to seed the database with its default values.
+k# This file should contain all the record creation needed to seed the database with its default values.
 # The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
 #
 # Examples:
@@ -11,11 +11,12 @@
 # Video.create(title: 'Monk', description: 'some thing funny', small_cover_url: '/tmp/monk_large.jpg', large_cover_url: '/tmp/monk.jpg')
 # Video.create(title: 'Futurama', description: 'some thing funny', small_cover_url: '/tmp/futurama.jpg', large_cover_url: '/tmp/futurama.jpg')
 
-Review.create(user_id: 6, video_id: 1, rating: 4, body: 'testing123')
-Review.create(user_id: 7, video_id: 1, rating: 4, body: 'testing123')
-Review.create(user_id: 12, video_id: 1, rating: 4,body: 'testing123')
+# Review.create(user_id: 6, video_id: 1, rating: 4, body: 'testing123')
+# Review.create(user_id: 7, video_id: 1, rating: 4, body: 'testing123')
+# Review.create(user_id: 12, video_id: 1, rating: 4,body: 'testing123')
 
-
+User.create(name: 'yufei', email: 'test@test.com', password: '12345')
+User.create(name: 'peter', email: 'test2@test.com', password: '12345')
 
 # Category.create(name: 'TV Show')
 
