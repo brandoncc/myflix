fbad28d0660b0c675a16e2ffd968b80390d56af9
figochen <figocia@hotmail.com>
2015-05-11 08:48:22 +1000

    staging env

--- app/mailers/app_mailer.rb
+++ app/mailers/app_mailer.rb
@@ -1,16 +1,16 @@
 class AppMailer < ActionMailer::Base 
   def welcome_letter(user)
     @user = user
-    mail from: 'info@myflix.com', to: user.email, subject: 'welcome to myflix'
+    mail from: 'info@myflix.com', to: Rails.env.staging? ? 'myflix@info.com' : user.email, subject: 'welcome to myflix'
   end
 
   def forgot_password(user)
     @user = user
-    mail from: 'info@myflix.com', to: user.email, subject: 'Resetting your password'
+    mail from: 'info@myflix.com', to: Rails.env.staging? ? 'myflix@info.com' : user.email, subject: 'Resetting your password'
   end
 
   def invitation(invitation)
     @invitation = invitation                
-    mail from: 'info@myflix.com', to: @invitation.email_invited, subject: "Invitation from #{@invitation.user.name}"
+    mail from: 'info@myflix.com', to: Rails.env.staging? ? 'myflix@info.com' : @invitation.email_invited, subject: "Invitation from #{@invitation.user.name}"
   end
 end
