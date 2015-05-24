4f9523df7d3c7c2c1d4461e2c041070aed7663a7
figochen <figocia@hotmail.com>
2015-05-25 08:55:07 +1000

    seeding staging env

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,19 +6,19 @@
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 
-# Video.create(title: 'SouthPark', description: 'some thing funny', small_cover_url: '/tmp/south_park.jpg', large_cover_url: '/tmp/south_park.jpg')
-# Video.create(title: 'Family guy', description: 'some thing funny', small_cover_url: '/tmp/family_guy.jpg', large_cover_url: '/tmp/family_guy.jpg')
-# Video.create(title: 'Monk', description: 'some thing funny', small_cover_url: '/tmp/monk_large.jpg', large_cover_url: '/tmp/monk.jpg')
-# Video.create(title: 'Futurama', description: 'some thing funny', small_cover_url: '/tmp/futurama.jpg', large_cover_url: '/tmp/futurama.jpg')
+Video.create(title: 'SouthPark', description: 'some thing funny', small_cover_url: '/tmp/south_park.jpg', large_cover_url: '/tmp/south_park.jpg')
+Video.create(title: 'Family guy', description: 'some thing funny', small_cover_url: '/tmp/family_guy.jpg', large_cover_url: '/tmp/family_guy.jpg')
+Video.create(title: 'Monk', description: 'some thing funny', small_cover_url: '/tmp/monk_large.jpg', large_cover_url: '/tmp/monk.jpg')
+Video.create(title: 'Futurama', description: 'some thing funny', small_cover_url: '/tmp/futurama.jpg', large_cover_url: '/tmp/futurama.jpg')
 
 # Review.create(user_id: 6, video_id: 1, rating: 4, body: 'testing123')
 # Review.create(user_id: 7, video_id: 1, rating: 4, body: 'testing123')
 # Review.create(user_id: 12, video_id: 1, rating: 4,body: 'testing123')
 
-User.create(name: 'yufei', email: 'test@test.com', password: '12345')
+User.create(name: 'yufei', email: 'admin@admin.com', password: '12345', admin: true) 
 User.create(name: 'peter', email: 'test2@test.com', password: '12345')
 
-# Category.create(name: 'TV Show')
+Category.create(name: 'TV Show')
 
 # VideoCategory.create(video_id: 1, category_id: 1)
 # VideoCategory.create(video_id: 2, category_id: 1)
